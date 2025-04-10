e1ae93c4b75cdd3a0651747a883cb45e20db3453
davemins <davemins4125@naver.com>
2025-04-10 11:11:52 +0900

    refactor: Todo 항목을 객체 배열로 구조 개선

---
+++ to-do-list/src/MapTest.tsx
@@ -0,0 +1,14 @@
+import React, { } from 'react';
+
+const MapTest = () => {
+    const fruits = ['apple', 'banana', 'orange']
+    
+    return(
+        <div>
+
+
+        </div>
+    )
+}
+
+export default MapTest;
--- to-do-list/src/ToDoList.tsx
+++ to-do-list/src/ToDoList.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 
+type Todo = {
+    id: number;
+    text: string;
+    isCheck: boolean;
+}
+
 const ToDoList: React.FC = () => {
     const title: string = "오늘 할 일";
 
-    const [todo, setTodo] = useState<string[]>(['오늘 할 일', '잠자기', '미팅하기']);
+    const [todo, setTodo] = useState<Todo[]>([
+        {id : 1, text : '공부하기', isCheck : false},
+        {id : 2, text : '잠자기', isCheck : false},
+        {id : 3, text : '미팅하기', isCheck : false}
+    ]);
 
     return (
         <div>
@@ -12,9 +22,10 @@ const ToDoList: React.FC = () => {
             <div className="container">
                 <div className="board">
                     <ul>
-                        <li>{todo[0]}</li>
-                        <li>{todo[1]}</li>
-                        <li>{todo[2]}</li>
+                        
+                        <li>{todo[0].text}</li>
+                        <li>{todo[1].text}</li>
+                        <li>{todo[2].text}</li>
                     </ul>
                 </div>
             </div>
