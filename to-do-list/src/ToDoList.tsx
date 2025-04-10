3dd32302f151b27891debfe60f8c2d9872981db2
davemins <davemins4125@naver.com>
2025-04-10 11:02:01 +0900

    Refactor: todo 배열에 setTodo 추가 및 클래스명(board) 적용

--- to-do-list/src/ToDoList.tsx
+++ to-do-list/src/ToDoList.tsx
@@ -3,18 +3,20 @@ import React, { useState } from "react";
 const ToDoList: React.FC = () => {
     const title: string = "오늘 할 일";
 
-    const [todo] = useState(['오늘 할 일', '잠자기', '미팅하기']);
+    const [todo, setTodo] = useState<string[]>(['오늘 할 일', '잠자기', '미팅하기']);
 
     return (
         <div>
             <h1>{title}</h1>
             <p></p>
             <div className="container">
-                <ul>
-                    <li>{todo[0]}</li>
-                    <li>{todo[1]}</li>
-                    <li>{todo[2]}</li>
-                </ul>
+                <div className="board">
+                    <ul>
+                        <li>{todo[0]}</li>
+                        <li>{todo[1]}</li>
+                        <li>{todo[2]}</li>
+                    </ul>
+                </div>
             </div>
         </div>
 
