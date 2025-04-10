38e90615e0ad98009f59de685ed4b15749a35cee
davemins <davemins4125@naver.com>
2025-04-10 11:16:37 +0900

    feat: map 함수 활용한 과일 리스트 컴포넌트 추가

--- to-do-list/src/MapTest.tsx
+++ to-do-list/src/MapTest.tsx
@@ -2,11 +2,17 @@ import React, { } from 'react';
 
 const MapTest = () => {
     const fruits = ['apple', 'banana', 'orange']
-    
-    return(
+
+    return (
         <div>
-
-
+            <h2>과일</h2>
+            <ul>
+                {
+                    fruits.map((fruit, index) => (
+                        <li key={index}>{fruit}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
